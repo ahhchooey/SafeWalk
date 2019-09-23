5d5eb99a25bf70245d00e9a998e38867cba734c6
Alex Chui <ahhchui@gmail.com>
2019-09-23 11:18:36 -0700

    added backend base setup

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const db = require('./config/keys').mongoURI;
 
 
 const app = express();
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World"));
 
---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://dev:42KfBd7SkE7RWjaQ@cluster0-yiyc3.mongodb.net/test?retryWrites=true&w=majority',
+  secretOrKey: '@kAs$J2~vIM2SwT'
+}
