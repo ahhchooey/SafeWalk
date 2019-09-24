f1ab5c606b3a259fcc151c503f8aa5be017969d1
Stanton Huang <stantonhuang11@gmail.com>
2019-09-23 17:10:38 -0700

    adds more seed data

--- Parse/index.js
+++ Parse/index.js
@@ -6,9 +6,9 @@ let crimes = [];
 crimes = json.map((ele) => ({
     latitude: ele.latitude.slice(0,10),
     longitude: ele.longitude.slice(0,10),
-    // date: ele.report_datetime,
-    // category: ele.incident_category,
-    // sub_category: ele.incident_subcategory,
+    date: ele.report_datetime,
+    category: ele.incident_category,
+    sub_category: ele.incident_subcategory,
     intersection: ele.intersection
 }))
 
@@ -23,7 +23,7 @@ crimes = json.map((ele) => ({
 map = crimes.filter(el => {
     // console.log(el);
     // (parseFloat(el.latitude) >= 37.7879) && (parseFloat(el.latitude) <= 37.7989) && (parseFloat(el.longitude) >= -122.4055) && (parseFloat(el.longitude) < -122.3965 )
-    return ((parseFloat(el.latitude) >= 37.7879) && (parseFloat(el.latitude) <= 37.7989) && (parseFloat(el.longitude) >= -122.4055) && (parseFloat(el.longitude) < -122.3965) && el.intersection.includes('BUSH'))
+    return ((parseFloat(el.latitude) >= 37.7879) && (parseFloat(el.latitude) <= 37.7989) && (parseFloat(el.longitude) >= -122.4055) && (parseFloat(el.longitude) < -122.3965) && el.intersection.includes('HALLECK'))
 })
 
 
