28f68d6e51ab38be9fa7ded60eaf717ea33dfaa6
Alex Chui <ahhchui@gmail.com>
2019-09-23 18:35:15 -0700

    initial node seed created

---
+++ node_seed.js
@@ -0,0 +1,201 @@
+{
+  1: {
+    id: 1,
+    latitude: 37.797858, 
+    longitude: -122.407051,
+    options: {
+        2: .03,
+        11: .11
+    },
+    name: "broadway // grant ave"
+  },
+  2: {
+    id: 2,
+    latitude: 37.797868,
+    longitude: -122.406694,
+    options: {
+        1: .03,
+        3: .17,
+        13: .14
+    },
+    name: "broadway // columbus st" 
+  },
+  3: {
+    id: 3,
+    latitude: 37.798020,
+    longitude: -122.405527,
+    options: {
+        2: .17,
+        4: .15,
+        13: .11
+    },
+    name: "broadway // kearny st"
+  },
+  4: {
+    id: 4,
+    latitude: 37.798270, 
+    longitude: -122.403877,
+    options: {
+        3: .15,
+        14: .11,
+        5: .03
+    },
+    name: "broadway // montgomery st"
+  },
+  5: {
+    id: 5,
+    latitude: 37.798284,
+    longitude: -122.403543,
+    options: {
+        4: .03,
+        6: .04
+    },
+    name: "broadway // bartol"
+  },
+  6: {
+    id: 6,
+    latitude: 37.798339,
+    longitude: -122.403091,
+    options: {
+        5: .04,
+        7: .08,
+        15: .1
+    },
+    name: "broadway // osgood st"
+  },
+  7: {
+    id: 7,
+    latitude: 37.7984297,
+    longitude: -122.40222,
+    options: {
+        6: .08,
+        8: .1,
+        16: .1
+    },
+    name: "broadway // sansome st"
+  },
+  8: {
+    id: 8,
+    latitude: 37.798590,
+    longitude: -122.401071,
+    options: {
+        7: .1,
+        9: .1
+        17: .11
+    },
+    name: "broadway // battery st"
+  },
+  9: {
+    id: 9,
+    latitude: 37.798738, 
+    longitude: -122.399897,
+    options: {
+        8: .1,
+        10: .11,
+        18: .11,
+    },
+    name: "broadway // front"
+  },
+  10: {
+    id: 10,
+    latitude: 37.7988809,
+    longitude: -122.39869,
+    options: {
+        9: .11,
+        29: .2,
+    },
+    name: "broadway // davis"
+  },
+  11: {
+    id: 11,
+    latitude: 37.796915,
+    longitude: -122.406857,
+    options: {
+        1: .11,
+        12: .03,
+    },
+    name: "pacific // grant"
+  },
+  12: {
+    id: 12,
+    latitude: 37.796948,
+    longitude: -122.406555,
+    options: {
+        11: .03,
+        20: .1
+    },
+    name: "pacific // beckett"
+  },
+  13: {
+    id: 13,
+    latitude: 37.797062, 
+    longitude: -122.405412,
+    options: {
+        2: .14,
+        3: .11,
+        14: .15,
+        22: .1,
+        35: .25
+    },
+    name: "pacific // columbus // kearny"
+  },
+  14: {
+    id: 14,
+    latitude: 37.797304, 
+    longitude: -122.403703,
+    options: {
+        4: .11,
+        13: .15,
+        15: .07,
+        24: .1
+    },
+    name: "pacific // montgomery"
+  },
+  15: {
+    id: 15,
+    latitude: 37.797414, 
+    longitude: -122.402899
+    options: {
+        6: .1,
+        14: .07,
+        16: .07
+    },
+    name: "pacific // osgood"
+  },
+  16: {
+    id: 16,
+    latitude: 37.797516, 
+    longitude: -122.402062, 
+    options: {
+        7: .1,
+        15: .07,
+        17: .1,
+        26: .1
+    },
+    name: "pacific // sansome"
+  },
+  17: {
+    id: 17,
+    latitude: 37.7976541,
+    longitude: -122.40086,
+    options: {
+        8: .11,
+        16: .1,
+        18: .1,
+        27: .1
+    },
+    name: "pacific // battery"
+  },
+  18: {
+    id: 18,
+    latitude: 37.7978066,
+    longitude: -122.39968,
+    options: {
+        9: .1,
+        17: .1,
+        28: .1
+    },
+    name: "pacific // front"
+  }
+}
+
