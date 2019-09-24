2402deddd4264f8c895abef45b9857b09c3a3e7b
Alex Chui <ahhchui@gmail.com>
2019-09-23 17:11:34 -0700

    intersection model and route

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const db = require('./config/keys').mongoURI;
+const intersections = require("./routes/api/intersections.js");
 
 
 const app = express();
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) => res.send({msg: "hello world"}));
+
+app.use("/api/intersections", intersections);
+app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
