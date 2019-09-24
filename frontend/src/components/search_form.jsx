048a20670cc01db56205444af4a70319a380b705
Theo <theodorejbrowne@gmail.com>
2019-09-23 21:47:19 -0700

    more search form

---
+++ frontend/src/components/search_form.jsx
@@ -0,0 +1,103 @@
+import React from 'react'
+import $ from 'jquery'
+import './stylesheets/search-form.scss'
+const mbxClient = require('@mapbox/mapbox-sdk'); 
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding')
+const baseClient = mbxClient({ accessToken: 'pk.eyJ1IjoidGhlby1icm93bmUiLCJhIjoiY2sweDNxNml3MDJrczNpcWk2Y2VkcTRscSJ9.spEwua1RKRltkkhpouI7-g' })
+const geocodingClient = mbxGeocoding(baseClient);
+
+
+export default class SearchForm extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            start: "",
+            destination: ""
+        }
+        this.selected = false
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.showDropdown = this.showDropdown.bind(this)
+    }
+    showDropdown(e){
+        e.target.nextElementSibling.classList.add('show')
+   
+    }
+    hideDropdown(e){
+        e.target.nextElementSibling.classList.remove('show')
+    }
+    handleClick(str, place){
+        this.setState({[str]: place })
+        debugger
+    }
+
+    handleInput(str) {
+        return (e) => {
+            this.setState({ [str]: e.target.value })
+            if (e.target.value === '') return
+            let val = e.target.value
+            geocodingClient.forwardGeocode({
+                query: val,
+                proximity: [-122.401672, 37.794418],
+                // bbox: [-122.398198, 37.798924, -122.395208, 37.794190],
+                  countries: ['us'],
+                limit: 5,
+                types: ['address']
+            }).send()
+                .then(res => {
+                    this.setState({ places: res.body.features})
+                })
+            // this.setState({ [str]: e.target.value })
+        }
+    }
+
+    handleSubmit(e) {
+        e.preventDefault()
+        let query = this.state
+        // $.ajax({
+        //     url: '/api/intersections',
+        //     method: 'GET',
+        //     data: {
+        //         query
+        //     }
+        // }).then(() => {
+        //     console.log('success')
+        //     this.setState({ start: "", destination: "" })
+        // }, () => {
+        //     console.log('failure')
+        // })
+    }
+
+
+    render(){
+        
+      let places = this.state.places || []
+        
+        return(
+            <div className="search-form">
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="">Start:
+                    <br/>
+                        <input type="text" onChange={this.handleInput('start')} onFocus={this.showDropdown} onBlur={this.hideDropdown} value={this.state.start} />
+                        <div className="locations-dropdown">
+                            <ul>
+                                {places.map(place => <li>{place.place_name}</li>)}
+                            </ul>
+                        </div>
+                    </label>
+                    <br/>
+                    <label onFocus={this.showDropdown} onBlur={this.hideDropdown} > Destination:
+                        <br/>
+                        <input type="text" onChange={this.handleInput('destination')} value={this.state.destination} />
+                        <div className="locations-dropdown">
+                            <ul>
+                                {places.map(place => <li onClick={console.log('test')} >{place.place_name}</li>)}
+                            </ul>
+                        </div>
+                    </label>
+                    <br/>
+                    <button type="submit" >Create Routes</button>
+                </form>
+            </div>
+        )
+    }
+}
--- frontend/src/components/splash.jsx
+++ frontend/src/components/splash.jsx
@@ -1,56 +1,22 @@
 import React from 'react'
-import $ from 'jquery'
+import SearchForm from './search_form'
+import './stylesheets/splash.scss'
 
 
 
 export default class Splash extends React.Component{
     constructor(props){
         super(props)
-        this.state = {
-            start: "",
-            destination: ""
-        }
-        this.handleSubmit = this.handleSubmit.bind(this)
     }
-
-    handleInput(str){
-        return (e) => {
-            this.setState({ [str]: e.target.value })
-        }
-    }
-
-    handleSubmit(e){
-        e.preventDefault()
-        let query = this.data
-        $.ajax({
-            url: '/api/intersections',
-            method: 'GET',
-            data: {
-               query
-            }
-        }).then(() => {
-            console.log('success')
-        }, () => {
-            console.log('failure')
-        })
-    }
-
 
     render(){
         return(
-            <div>
-                <h1>
-                    Splash
-                </h1>
-                <form onSubmit={this.handleSubmit}> 
-                    <label htmlFor="">Start 
-                        <input type="text" onChange={this.handleInput('start')}/>
-                    </label>
-                    <label > Destination
-                        <input type="text" onChange={this.handleInput('destination')} />
-                    </label>
-                    <button type="submit" >Create Routes</button>
-                </form>
+          
+            <div className="splash">
+                <div>
+                <h2>Welcome to SafeWalk</h2>
+                <SearchForm />
+                </div>
             </div>
         )
     }
