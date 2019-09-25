816959368f8ab9f5f8f644a38a06ea545a307c0b
Stanton Huang <stantonhuang11@gmail.com>
2019-09-25 16:42:52 -0700

    adds geolocation

--- Parse/geolocation.js
+++ Parse/geolocation.js
@@ -1,19 +1,51 @@
-const options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0
-};
+const seed = require('../node_seed');
+// import seed from '../node_seed';
 
-function success(pos) {
-    let crd = pos.coords;
+// const options = {
+//     enableHighAccuracy: true,
+//     timeout: 5000,
+//     maximumAge: 0
+// };
 
-    console.log('Your current position is:');
-    console.log(`Latitude : ${crd.latitude}`);
-    console.log(`Longitude: ${crd.longitude}`);
+
+// latitude: 37.7988017
+// Longitude: -122.40145079999999
+// let temp;
+
+// function success(pos) {
+//     let crd = pos.coords;
+//     temp = pos
+//     console.log(crd);
+//     console.log('Your current position is:');
+//     console.log(`Latitude : ${crd.latitude}`);
+//     console.log(`Longitude: ${crd.longitude}`);
+// }
+
+// function error(err) {
+//     console.warn(`ERROR(${err.code}): ${err.message}`);
+// }
+
+function closest(pos) {
+    let blah = [];
+    console.log(pos);
+    for (let intersection in Object.values(seed)) {
+        temp = intersection.toString();
+        // console.log(typeof temp);
+        // console.log(temp);
+        // console.log(intersection === temp);
+        // console.log(Object.values(seed)[temp])
+        let dist = Math.pow((Math.pow((pos.latitude - Object.values(seed)[temp].latitude), 2) + Math.pow((pos.longitude - Object.values(seed)[temp].longitude), 2)), .5);
+        blah.push(dist)
+        console.log(dist)
+        // console.log(seed[intersection]);
+        // console.log(dist);
+    }
+    console.log(blah);
+    // let bloo = Math.min(blah)
+    console.log(Math.min(...blah))
 }
 
-function error(err) {
-    console.warn(`ERROR(${err.code}): ${err.message}`);
-}
+let pos =  {latitude: 37.7987445, longitude: -122.40145999999999}
 
-navigator.geolocation.getCurrentPosition(success, error, options);
+// navigator.geolocation.getCurrentPosition(success, error, options);
+closest(pos);
