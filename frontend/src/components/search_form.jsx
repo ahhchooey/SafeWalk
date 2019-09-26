efedabc78117e7c560e20c04ac91c548adb33680
Alex Chui <ahhchui@gmail.com>
2019-09-26 13:00:04 -0700

    made input fields required

--- frontend/src/components/search_form.jsx
+++ frontend/src/components/search_form.jsx
@@ -102,7 +102,14 @@ export default class SearchForm extends React.Component {
 
                     <label htmlFor="">From:
                     <br />
-                        <input type="text" onChange={this.handleInput('start')} onFocus={() => this.showDropdown('start')} onBlur={() => this.hideDropdown('start')} value={this.state.start} />
+                    <input 
+                      type="text" 
+                      required 
+                      onChange={this.handleInput('start')} 
+                      onFocus={() => this.showDropdown('start')} 
+                      onBlur={() => this.hideDropdown('start')} 
+                      value={this.state.start} 
+                    />
                         <div className="locations-dropdown">
                             <ul>
                                 {this.state.startPlaces.map((place, idx) => <li key={idx} onPointerDown={() => this.handleClick('start', place)}>{place.place_name}</li>)}
@@ -112,7 +119,12 @@ export default class SearchForm extends React.Component {
                     <br />
                     <label onFocus={() => this.showDropdown('destination')} onBlur={() => this.hideDropdown('destination')} > To:
                         <br />
-                        <input type="text" onChange={this.handleInput('destination')} value={this.state.destination} />
+                        <input 
+                          type="text" 
+                          required
+                          onChange={this.handleInput('destination')} 
+                          value={this.state.destination} 
+                        />
                         <div className="locations-dropdown">
                             
                         </div>
