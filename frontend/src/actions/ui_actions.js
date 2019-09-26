e7cc35c10a2c7f875e60d5bc33adc7e7dac83bde
Stanton Huang <stantonhuang11@gmail.com>
2019-09-26 10:20:02 -0700

    standardizes capitalization

--- frontend/src/actions/ui_actions.js
+++ frontend/src/actions/ui_actions.js
@@ -3,18 +3,18 @@ export const TOGGLE_ALL_DIRECTIONS = "TOGGLE_ALL_DIRECTIONS";
 export const TOGGLE_TRIP_INFO = "TOGGLE_TRIP_INFO";
 export const TOGGLE_TURN_BY_TURN = "TOGGLE_TURN_BY_TURN";
 
-export const toggle_show_search = () => ({
+export const toggleShowSearch = () => ({
     action: TOGGLE_SHOW_SEARCH
 })
 
-export const toggle_all_directions = () => ({
+export const toggleAllDirections = () => ({
     action: TOGGLE_ALL_DIRECTIONS
 })
 
-export const toggle_trip_info = () => ({
+export const toggleTripInfo = () => ({
     action: TOGGLE_TRIP_INFO
 })
 
-export const toggle_turn_by_turn = () => ({
+export const toggleTurnByTurn = () => ({
     action: TOGGLE_TURN_BY_TURN
 })
