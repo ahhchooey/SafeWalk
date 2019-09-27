42e5c77d000f9771cb2892b12e4697f50f52eee8
Stanton Huang <stantonhuang11@gmail.com>
2019-09-27 15:33:30 -0700

    prep for heroku

--- app.js
+++ app.js
@@ -24,11 +24,11 @@ app.get("/", (req, res) => res.send(seed));
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
-
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
   app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   })
 }
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
