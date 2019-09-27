2eca48eaee462267cca8ee31afaa9e00a09cebad
Stanton Huang <stantonhuang11@gmail.com>
2019-09-27 15:30:36 -0700

    prep for heroku

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const db = require('./config/keys').mongoURI;
 const intersections = require("./routes/api/intersections.js");
 const Intersection = require("./models/intersection.model.js");
 const seed = require("./node_seed.js");
-
+const path = require('path');
 
 const app = express();
 mongoose
@@ -25,3 +25,10 @@ app.get("/", (req, res) => res.send(seed));
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
